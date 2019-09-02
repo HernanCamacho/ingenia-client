25c82e21211d6c3b0682e774ad8ecf0449fdb95b
Hernan Camacho <hcamacho@ucol.mx>
2019-09-02 00:31:32 -0500

    Updated LoginComponent

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,14 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
     selector: 'login',
     templateUrl: './login.component.html',
-    providers: []
+    providers: [UserService]
 })
 export class LoginComponent implements OnInit {
 
+    public title: string;
+    public user: User;
+    public token: string;
+    public identity;
+    public status: boolean;
 
-    constructor(){}
+    constructor(
+         private _userService: UserService,
+         private _router: Router
+    ) {
+        this.title = "Es momento de entrar a la aplicación";
+        this.user = new User("","","","","");
+    }
 
-    ngOnInit(){}
+    ngOnInit() {
+        console.log(this.title);
+    }
+
+    login(form) {
+        this._userService.login(this.user).subscribe(
+            response => {
+                if(response && response.access_token){
+                    this.token = response.access_token;
+                    this.identity = response.identity;
+                    if(this._userService.persistData(this.token, this.identity)){
+                        this._router.navigate(['/usuarios']);
+                    }else if(response.message){
+                        this.title = response.message;
+                    }
+                }
+            }, error => {
+                let errorMessage = <any>error;
+                if(errorMessage != null){
+                    this.status = true;
+                }
+                console.log(errorMessage);
+            });
+    }
 }
