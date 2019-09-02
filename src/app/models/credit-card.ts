703557ed1e3596bad465048f27beff54f523b097
Hernan Camacho <hcamacho@ucol.mx>
2019-09-01 23:02:29 -0500

    Added models

---
+++ src/app/models/credit-card.ts
@@ -0,0 +1,6 @@
+export class CreditCard {
+    constructor(
+        public credit_card_number: string,
+        public user_id: string
+    ){}
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    constructor(
+        public name: string,
+        public surname: string,
+        public email: string,
+        public phone_number: string,
+        public password: string
+    ){}
+}
